700c5eb15d51f172d60547f3992ffd433ad239bd
ToRdroid <tiphone89@googlemail.com>
2017-11-08 15:15:33 +0100

    Update aosp_zerolteskt.mk

--- aosp_zerolteskt.mk
+++ aosp_zerolteskt.mk
@@ -8,7 +8,7 @@ TARGET_SCREEN_WIDTH := 1440
 # $(call inherit-product, vendor/lineage/config/nfc_enhanced.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="zerolteskt" \
